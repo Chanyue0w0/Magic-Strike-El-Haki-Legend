38de66728cd97afcaa03f3372974737faedc699b
Chanyue0w0 <hunnil.ttp@gmail.com>
2025-03-05 11:08:34 +0800

    feat FireShield
    feat FireShield

--- Assets/Scripts/FightGame/Skills/FlamingStar.cs
+++ Assets/Scripts/FightGame/Skills/FlamingStar.cs
@@ -8,6 +8,7 @@ public class FlamingStar : MonoBehaviour
     //[SerializeField] private float OriginalShieldPercentage = 0.0f;
     [SerializeField] private int skillDamage = 100;
     [SerializeField] private int skillMoveSpeed = 10;
+    [SerializeField] private GameObject chargeFireShield;
     [SerializeField] private GameObject flamingStarObj;
 
     [SerializeField] private GameObject player1;
@@ -33,6 +34,7 @@ public class FlamingStar : MonoBehaviour
         }
         skillDamage = Mathf.RoundToInt(skillDamageTMP);
         flamingStarObj = Resources.Load<GameObject>("Prefabs/Skills/FlamingStar");
+        chargeFireShield = Resources.Load<GameObject>("Prefabs/Effect/FireShield");
         player1 = GameObject.FindGameObjectWithTag("Player1");
         player2 = GameObject.FindGameObjectWithTag("Player2");
     }
@@ -41,6 +43,26 @@ public class FlamingStar : MonoBehaviour
     {
        
         if (playerNumber == 1)
+        {
+            GameObject CFS = Instantiate(chargeFireShield, player1.transform.position, Quaternion.identity);
+            CFS.transform.SetParent(player1.transform);
+            StartCoroutine(DelayedInstFlamingStar(1, 0.8f));
+        }
+        else
+        {
+            GameObject CFS = Instantiate(chargeFireShield, player2.transform.position, Quaternion.identity);
+            CFS.transform.SetParent(player2.transform);
+            StartCoroutine(DelayedInstFlamingStar(2, 0.8f));
+        }
+
+        
+    }
+
+
+    private IEnumerator DelayedInstFlamingStar(int pNumber ,float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if(pNumber == 1)
         {
             GameObject obj = Instantiate(flamingStarObj, player1.transform.position, Quaternion.identity);
             obj.GetComponent<AimPlayerShootSkill>().SetPlayerNumber(playerNumber);
@@ -54,8 +76,7 @@ public class FlamingStar : MonoBehaviour
             obj.GetComponent<AimPlayerShootSkill>().SetTargetDirection(player1.transform.position);
             obj.GetComponent<AimPlayerShootSkill>().SetSkillDamage(skillDamage);
         }
-
-        
     }
+
 
 }
