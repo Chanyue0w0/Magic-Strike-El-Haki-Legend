5ddaf6e0b4028b71f3a655b89986dd3901704fd3
Chanyue0w0 <hunnil.ttp@gmail.com>
2025-03-28 01:15:14 +0800

    feat MainMenu & StartScene Music
    feat MainMenu & StartScene Music

--- Assets/Scripts/StartScene/StartSceneManager.cs
+++ Assets/Scripts/StartScene/StartSceneManager.cs
@@ -8,7 +8,7 @@ public class StartSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		AudioManager.Instance.PlayBGM(MusicAudioClips.Instance.HallBGM);
+		AudioManager.Instance.PlayBGM(MusicAudioClips.Instance.MainBGM);
 
         PlayerEquipmentManager.Instance.LoadEquipment();
     }
