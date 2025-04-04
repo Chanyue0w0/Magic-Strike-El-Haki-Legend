f574f69589e41b04dc4a2bbe7fb0818021773048
RogeWood <a813712100@gmail.com>
2025-04-05 03:39:34 +0800

    fix: battle caculate player 1 config skill input not refresh

--- Assets/Scripts/MainMenu/BattleDataCalculator.cs
+++ Assets/Scripts/MainMenu/BattleDataCalculator.cs
@@ -103,6 +103,10 @@ public class BattleDataCalculator : MonoBehaviour
 		totalHP = (int)((baseHP + equipmentTotalHP) * totalHPBuff);
 		totalATK = (int)((baseATK + equipmentTotalATK) * totalATKBuff);
 		setEffect = DetermineSetEffect(setTypeCount);
+
+
+		skill_1 = (skill_1 == null) ? skill_2 : skill_1;
+		skill_2 = (skill_2 == null) ? skill_1 : skill_2;
 	}
 
 
@@ -118,8 +122,8 @@ public class BattleDataCalculator : MonoBehaviour
 		FightPlayer1Config.CC_SkillTimeIncrease = controlSkillDurationIncrease;
 		FightPlayer1Config.SkillBubbleTimeDecrease = totalSkillBubbleCooldownReduction;
 		FightPlayer1Config.EquipSet = setEffect;
-		FightPlayer1Config.Group[1] = (skill_1 == null) ? skill_2 : skill_1;
-		FightPlayer1Config.Group[2] = (skill_2 == null) ? skill_1 : skill_2;
+		FightPlayer1Config.Group[1] = skill_1;
+		FightPlayer1Config.Group[2] = skill_2;
 		if (FightPlayer1Config.Group[1] == null && FightPlayer1Config.Group[2] == null)
 		{
 			FightPlayer1Config.Group[1] = "SK00";
